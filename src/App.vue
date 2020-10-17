9f1f6015a0b46e6f7a1e1e599e4619258b4a2086
Paul Schaaf <paulsimonschaaf@gmail.com>
2020-10-17 22:33:49 +0200

    removed dummy code

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <Creator/>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Creator from './components/Creator.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    Creator
   }
 });
 </script>
