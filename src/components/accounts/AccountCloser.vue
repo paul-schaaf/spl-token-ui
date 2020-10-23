e54529f22f7435f12a7affd1d125dd29b4f0ef42
Paul Schaaf <paulsimonschaaf@gmail.com>
2020-10-23 18:32:07 +0200

    [feat] prev commit did not include AccountCloser component

---
+++ src/components/accounts/AccountCloser.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="field">
+    <label class="label">Account address*</label>
+    <div class="control">
+      <input
+        v-model="accountAddress"
+        class="input is-black"
+        type="text"
+        placeholder="Public Key String e.g. GsbwXfJraMomNxBcjYLcG3mxkBUiyWXAB32fGbSMQRdW"
+      />
+    </div>
+  </div>
+  <div class="field">
+    <label class="label"> Owner*</label>
+    <div class="control">
+      <input
+        v-model="owner"
+        class="input is-black"
+        type="text"
+        placeholder="Seed seed phrase"
+      />
+    </div>
+    <p class="help">
+      Your secret phrase is NOT saved NOR sent anywhere. It's only used to sign
+      the owner change request fee.
+    </p>
+  </div>
+  <div class="field">
+    <label class="label">Destination account*</label>
+    <div class="control">
+      <input
+        v-model="destinationAccount"
+        class="input is-black"
+        type="text"
+        placeholder="Public Key String e.g. GsbwXfJraMomNxBcjYLcG3mxkBUiyWXAB32fGbSMQRdW"
+      />
+    </div>
+  </div>
+  <div style="display: flex" class="control is-justify-content-center mt-5">
+    <button
+      :class="{ 'is-loading': closingAccount }"
+      class="button is-black"
+      @click="onCloseAccount"
+    >
+      Close account
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, toRefs } from "vue";
+import accountComponents from "./accountComponents";
+import { closeAccount } from "@/solana/token";
+
+export default defineComponent({
+  name: accountComponents.Close,
+  props: {
+    payerSeedPhrase: {
+      type: String,
+      required: true
+    },
+    tokenAddress: {
+      type: String,
+      required: true
+    }
+  },
+  setup(props, { emit }) {
+    const { payerSeedPhrase, tokenAddress } = toRefs(props);
+    const closingAccount = ref(false);
+    const accountAddress = ref("");
+    const owner = ref("");
+    const destinationAccount = ref("");
+
+    const onCloseAccount = async () => {
+      closingAccount.value = true;
+      emit("update:accountAddress", "");
+      try {
+        await closeAccount(
+          payerSeedPhrase.value,
+          tokenAddress.value,
+          accountAddress.value,
+          destinationAccount.value,
+          owner.value
+        );
+        emit("update:accountAddress", accountAddress.value);
+      } catch (err) {
+        alert(err);
+      }
+
+      closingAccount.value = false;
+    };
+
+    return {
+      closingAccount,
+      accountAddress,
+      onCloseAccount,
+      owner,
+      destinationAccount
+    };
+  }
+});
+</script>
