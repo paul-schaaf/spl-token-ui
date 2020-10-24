54eaa399ed968ae6fda68eb76c4b804551290263
Paul Schaaf <paulsimonschaaf@gmail.com>
2020-10-24 18:09:16 +0200

    [fix] fix bad phone ui by not allowing phones screen sizes 🙃

--- src/App.vue
+++ src/App.vue
@@ -1,106 +1,116 @@
 <template>
-  <nav class="navbar is-black" role="navigation" aria-label="main navigation">
-    <div class="container">
-      <div class="navbar-menu">
-        <div class="navbar-start">
-          <a class="navbar-item">
-            <strong>SPL TOKEN UI</strong>
-          </a>
-          <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link">
-              Tokens
+  <template v-if="!isOnMobile"
+    ><nav
+      class="navbar is-black"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="container">
+        <div class="navbar-menu">
+          <div class="navbar-start">
+            <a class="navbar-item">
+              <strong>SPL TOKEN UI</strong>
             </a>
+            <div class="navbar-item has-dropdown is-hoverable">
+              <a class="navbar-link">
+                Tokens
+              </a>
 
-            <div class="navbar-dropdown">
-              <router-link :to="{ name: 'token-creator' }">
-                <a
-                  :class="{
-                    'has-background-light': 'token-creator' === $route.name,
-                    'has-text-black': 'token-creator' === $route.name
-                  }"
-                  class="navbar-item"
-                >
-                  Create new token
-                </a>
-              </router-link>
-              <router-link :to="{ name: 'token-editor' }">
-                <a
-                  :class="{
-                    'has-background-light': 'token-editor' === $route.name,
-                    'has-text-black': 'token-editor' === $route.name
-                  }"
-                  class="navbar-item"
-                >
-                  Edit existing token
-                </a>
-              </router-link>
+              <div class="navbar-dropdown">
+                <router-link :to="{ name: 'token-creator' }">
+                  <a
+                    :class="{
+                      'has-background-light': 'token-creator' === $route.name,
+                      'has-text-black': 'token-creator' === $route.name
+                    }"
+                    class="navbar-item"
+                  >
+                    Create new token
+                  </a>
+                </router-link>
+                <router-link :to="{ name: 'token-editor' }">
+                  <a
+                    :class="{
+                      'has-background-light': 'token-editor' === $route.name,
+                      'has-text-black': 'token-editor' === $route.name
+                    }"
+                    class="navbar-item"
+                  >
+                    Edit existing token
+                  </a>
+                </router-link>
+              </div>
+            </div>
+            <div class="navbar-item has-dropdown is-hoverable">
+              <a class="navbar-link">
+                Accounts
+              </a>
+
+              <div class="navbar-dropdown">
+                <router-link :to="{ name: 'account-creator' }">
+                  <a
+                    :class="{
+                      'has-background-light': 'account-creator' === $route.name,
+                      'has-text-black': 'account-creator' === $route.name
+                    }"
+                    class="navbar-item"
+                  >
+                    Create Account
+                  </a>
+                </router-link>
+                <router-link :to="{ name: 'account-editor' }">
+                  <a
+                    :class="{
+                      'has-background-light': 'account-editor' === $route.name,
+                      'has-text-black': 'account-editor' === $route.name
+                    }"
+                    class="navbar-item"
+                  >
+                    Edit Account
+                  </a>
+                </router-link>
+              </div>
             </div>
           </div>
-          <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link">
-              Accounts
-            </a>
-
-            <div class="navbar-dropdown">
-              <router-link :to="{ name: 'account-creator' }">
-                <a
-                  :class="{
-                    'has-background-light': 'account-creator' === $route.name,
-                    'has-text-black': 'account-creator' === $route.name
-                  }"
-                  class="navbar-item"
-                >
-                  Create Account
-                </a>
-              </router-link>
-              <router-link :to="{ name: 'account-editor' }">
-                <a
-                  :class="{
-                    'has-background-light': 'account-editor' === $route.name,
-                    'has-text-black': 'account-editor' === $route.name
-                  }"
-                  class="navbar-item"
-                >
-                  Edit Account
-                </a>
-              </router-link>
-            </div>
-          </div>
-        </div>
-        <div class="navbar-end">
-          <div class="navbar-item">
-            <a
-              id="source-code"
-              href="https://github.com/paul-schaaf/spl-token-ui"
-              target="_blank"
-              rel="noopener noreferrer"
-              >source code</a
-            >
-          </div>
-          <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link">
-              {{ chosenCluster }}
-            </a>
-
-            <div class="navbar-dropdown is-right">
+          <div class="navbar-end">
+            <div class="navbar-item">
               <a
-                v-for="network in NETWORKS"
-                :key="network"
-                :class="{
-                  'has-background-light': network === chosenCluster,
-                  'has-text-black': network === chosenCluster
-                }"
-                class="navbar-item"
-                @click="changeCluster(network)"
-                >{{ network }}</a
+                id="source-code"
+                href="https://github.com/paul-schaaf/spl-token-ui"
+                target="_blank"
+                rel="noopener noreferrer"
+                >source code</a
               >
+            </div>
+            <div class="navbar-item has-dropdown is-hoverable">
+              <a class="navbar-link">
+                {{ chosenCluster }}
+              </a>
+
+              <div class="navbar-dropdown is-right">
+                <a
+                  v-for="network in NETWORKS"
+                  :key="network"
+                  :class="{
+                    'has-background-light': network === chosenCluster,
+                    'has-text-black': network === chosenCluster
+                  }"
+                  class="navbar-item"
+                  @click="changeCluster(network)"
+                  >{{ network }}</a
+                >
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </nav>
-  <router-view></router-view>
+    </nav>
+    <router-view></router-view
+  ></template>
+  <p v-else>
+    This app does not work on phones. Please switch to a device with a larger
+    screen.
+  </p>
 </template>
 
 <script lang="ts">
@@ -112,7 +122,12 @@ export default {
   setup() {
     changeCluster(chosenCluster.value);
 
-    return { NETWORKS, chosenCluster, changeCluster };
+    return {
+      NETWORKS,
+      chosenCluster,
+      changeCluster,
+      isOnMobile: window.outerWidth < 600
+    };
   }
 };
 </script>
