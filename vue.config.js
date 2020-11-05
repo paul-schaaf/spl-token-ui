b68ecc4d60da63f138f055198845a2520dafdc79
Paul Schaaf <paulsimonschaaf@gmail.com>
2020-11-06 00:23:14 +0100

    linting

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,14 @@
 // vue.config.js
 module.exports = {
-    chainWebpack: config => {
-      config.module
-        .rule('fonts')
-        .use('url-loader')
-          .loader('url-loader')
-          .tap(options => {
-            options.fallback.options.name = 'fonts/[name].[ext]';
-            // modify the options...
-            return options
-          })
-    }
+  chainWebpack: config => {
+    config.module
+      .rule("fonts")
+      .use("url-loader")
+      .loader("url-loader")
+      .tap(options => {
+        options.fallback.options.name = "fonts/[name].[ext]";
+        // modify the options...
+        return options;
+      });
   }
+};
