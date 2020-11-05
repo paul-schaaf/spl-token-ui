ab78e782e57343b05c13f3d99415dbf03ebc7f6d
Paul Schaaf <paulsimonschaaf@gmail.com>
2020-11-05 23:35:00 +0100

    adjust webpack config so preloading becomes possible

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+// vue.config.js
+module.exports = {
+    chainWebpack: config => {
+      config.module
+        .rule('fonts')
+        .use('url-loader')
+          .loader('url-loader')
+          .tap(options => {
+            options.fallback.options.name = 'fonts/[name].[ext]';
+            // modify the options...
+            return options
+          })
+    }
+  }
