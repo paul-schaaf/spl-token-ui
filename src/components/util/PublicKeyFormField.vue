b89f807a4854b32b9fdbe2e7b9a52f3802445285
Paul Schaaf <paulsimonschaaf@gmail.com>
2020-11-17 19:30:04 +0100

    add PublicKeyFormField

---
+++ src/components/util/PublicKeyFormField.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="control">
+    <input
+      :value="address"
+      class="input is-black"
+      type="text"
+      placeholder="Public Key String e.g. GsbwXfJraMomNxBcjYLcG3mxkBUiyWXAB32fGbSMQRdW"
+      @input="$emit('update:address', $event.target.value)"
+    />
+  </div>
+  <div class="help">
+    <span v-if="derivePublicKey"
+      ><span class="is-clickable" @click="onDerivePublicKey"
+        ><strong>Derive public key from external wallet</strong></span
+      >{{ hint }}</span
+    >
+  </div>
+</template> 
+
+<script lang="ts">
+import { useWallet } from "@/solana/externalWallet";
+import { defineComponent } from "vue";
+export default defineComponent({
+  emits: ["update:address"],
+  props: {
+    address: {
+      type: String,
+      default: ""
+    },
+    derivePublicKey: {
+      type: Boolean,
+      default: false
+    },
+    hint: {
+      type: String,
+      default: ""
+    }
+  },
+  setup(_, { emit }) {
+    const onDerivePublicKey = async () => {
+      const wallet = await useWallet();
+      emit("update:address", wallet.publicKey.toBase58());
+    };
+
+    return { onDerivePublicKey };
+  }
+});
+</script>
