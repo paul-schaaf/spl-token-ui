bafdfe8d06eac64287d27f4d490b44d85db33e75
Paul Schaaf <paulsimonschaaf@gmail.com>
2020-11-17 20:02:03 +0100

    add public key form field to AccountThawer

--- src/components/accounts/accountEditing/AccountThawer.vue
+++ src/components/accounts/accountEditing/AccountThawer.vue
@@ -8,14 +8,7 @@
   </div>
   <div class="field">
     <label class="label">Account to thaw*</label>
-    <div class="control">
-      <input
-        v-model="accountToThawAddress"
-        class="input is-black"
-        type="text"
-        placeholder="Public Key String e.g. GsbwXfJraMomNxBcjYLcG3mxkBUiyWXAB32fGbSMQRdW"
-      />
-    </div>
+    <public-key-form-field v-model:address="accountToThawAddress" />
   </div>
   <div style="display: flex" class="control is-justify-content-center mt-5">
     <button
@@ -33,10 +26,13 @@ import { defineComponent, ref, toRefs } from "vue";
 import accountComponents from "../accountComponents";
 import { thawAccount } from "@/solana/token";
 import SecretFormField from "@/components/util/SecretFormField.vue";
+import PublicKeyFormField from "@/components/util/PublicKeyFormField.vue";
+
 export default defineComponent({
   name: accountComponents.Thaw,
   components: {
-    SecretFormField
+    SecretFormField,
+    PublicKeyFormField
   },
   emits: ["update:accountAddress"],
   props: {
@@ -44,7 +40,6 @@ export default defineComponent({
       type: String,
       required: true
     },
-
     payerSignsExternally: {
       type: Boolean,
       default: true
