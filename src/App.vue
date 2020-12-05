f23704d1a8c11bf40f94d59326b37dcd23340e9b
Paul Schaaf <paulsimonschaaf@gmail.com>
2020-12-05 17:32:14 +0100

    fix issue #1

--- src/App.vue
+++ src/App.vue
@@ -176,7 +176,7 @@
 </template>
 
 <script lang="ts">
-import { computed } from "vue";
+import { computed, onMounted } from "vue";
 import { CLUSTERS } from "./solana/connection";
 import { changeCluster, chosenCluster } from "./solana/connection";
 
@@ -206,6 +206,26 @@ export default {
           break;
       }
       return chosenCluster.value + " " + emoji;
+    });
+
+    onMounted(() => {
+      document
+        .getElementsByClassName("navbar")[0]
+        .querySelectorAll(".has-dropdown")
+        .forEach(el => {
+          el.addEventListener("click", () => {
+            let menu = el.querySelector(".navbar-dropdown");
+            //@ts-expect-error
+            menu.style.display = "none";
+            setTimeout(() => {
+              //@ts-expect-error
+              el.blur();
+              // Reset the display property to its original state, so the menu can appear again next time
+              //@ts-expect-error
+              menu.style.display = "";
+            }, 200);
+          });
+        });
     });
 
     return {
